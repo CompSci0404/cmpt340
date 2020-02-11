693b63b9f33e98e0873643338b8206057d83bfed
Trooper0404 <mtradke@sasktel.net>
2020-02-10 19:52:33 -0600

    finished question 1A

--- src/assignment2/FaroShuffle.scala
+++ src/assignment2/FaroShuffle.scala
@@ -1,11 +1,60 @@
 package assignment2
 
+//Matt Radke
+// 11196547
+// mmr174
+
 object FaroShuffle {
 
 
+  def shuffle[A](L1:List[A], L2:List[A]): List[A] = {
+    var newList = List[A]()
+
+    //Create a function that checks the length of Both L1 and L2
+    def lenChoice(x: Any) = x match {
+      case x: Int if x >= L2.length => x
+      case x: Int if x < L2.length => L2.length
+    }
+
+    // exception handler.
+    def getListItem(ls:List[A], n:Int): Option[List[A]] = {
+
+      // if n is bigger then the length of  list using we will return NONE
+      // else we use option.
+      if(ls.length > n) {
+        newList ::= ls(n) //creates a NEW list, and appends the old content to the front of it.
+
+        Some(newList)
+
+      } else {
+
+        None
+      }
+
+    }
+
+
+    def loop(n:Int): Int = {
+      getListItem(L1, n)
+      getListItem(L2, n)
+
+      if(n == (lenChoice(L1.length)-1))1
+      else loop(n + 1)
+
+    }
+
+    loop(0)
+
+    newList.reverse // return the final list once we are finished with it!
+  }
+
   def main(args:Array[String]): Unit = {
 
-      println("yeet")
+    val num1: List[String] = List("h1", "h2", "h3")
+    val num2: List[String] = List("c1", "c2", "c3", "c4", "c5", "c6")
+
+    println(shuffle(num1, num2))
+
   }
 
 }
